5f8c091fad08243b054250f2636f298f0cb15fe8
dcostap <dariocostapecalama@gmail.com>
2026-09-21 10:43:41 +0200

    Set centered editor width to 1300

--- data/plugins/anvil_defaults.lua
+++ data/plugins/anvil_defaults.lua
@@ -115,7 +115,7 @@ plugin_defaults("navigation_history", {
 })
 plugin_defaults("centered_editor", {
   enabled = true,
-  max_width = 1400,
+  max_width = 1300,
   markdown_live_max_width = 800,
   scale_width = true,
   min_margin = 0,
